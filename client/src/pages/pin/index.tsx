bf8327bd9e09a321dadd7f453cd34daee2c71767
duybinh2311 <duybinh.nguyen2311@gmail.com>
2023-10-16 16:40:01 +0700

    update

--- client/src/pages/pin/index.tsx
+++ client/src/pages/pin/index.tsx
@@ -1,9 +1,11 @@
 import { vars } from '@/configs/theme'
-import { Box, Container, Image, ScrollArea, SimpleGrid } from '@mantine/core'
+import { Box, Container, Image, ScrollArea, SimpleGrid, Text } from '@mantine/core'
 import { FC } from 'react'
 import { PinActionMenu } from './components/pin-action-menu'
 import { PinCommentBox } from './components/pin-comment-box'
 import { PinDescription } from './components/pin-description'
+import { PinLayout } from '@/components/pin-layout'
+import { Pin } from '@/components/pin'
 
 export const PinPage: FC = () => {
   return (
@@ -24,16 +26,14 @@ export const PinPage: FC = () => {
               <Image
                 height={'100%'}
                 width={'100%'}
-                src={
-                  'https://i.pinimg.com/564x/e6/22/ce/e622ce555bcb0ac835a98027868934f3.jpg'
-                }
+                src={'https://i.pinimg.com/564x/e6/22/ce/e622ce555bcb0ac835a98027868934f3.jpg'}
               />
             </Box>
             <Box>
               <Box className='pin-description'>
                 <PinActionMenu />
 
-                <ScrollArea h={400}>
+                <ScrollArea h={'calc(100vh - 325px'}>
                   <PinDescription />
                 </ScrollArea>
               </Box>
@@ -43,6 +43,45 @@ export const PinPage: FC = () => {
               </Box>
             </Box>
           </SimpleGrid>
+        </Container>
+      </section>
+
+      <section
+        style={{
+          paddingTop: vars.spacing.xl,
+        }}
+      >
+        <Container fluid>
+          <Text
+            ta={'center'}
+            fw={500}
+            fz={'xl'}
+          >
+            More to explore
+          </Text>
+          <PinLayout>
+            <Pin size='small' />
+            <Pin size='medium' />
+            <Pin size='large' />
+            <Pin size='small' />
+            <Pin size='medium' />
+            <Pin size='large' />
+            <Pin size='small' />
+            <Pin size='medium' />
+            <Pin size='large' />
+            <Pin size='small' />
+            <Pin size='medium' />
+            <Pin size='large' />
+            <Pin size='small' />
+            <Pin size='medium' />
+            <Pin size='large' />
+            <Pin size='small' />
+            <Pin size='medium' />
+            <Pin size='large' />
+            <Pin size='small' />
+            <Pin size='medium' />
+            <Pin size='large' />
+          </PinLayout>
         </Container>
       </section>
     </>
