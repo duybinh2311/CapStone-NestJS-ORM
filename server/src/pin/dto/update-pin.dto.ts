aae45ef41725af99009fc1260662d2192213ecf3
duybinh2311 <duybinh.nguyen2311@gmail.com>
2023-11-04 05:28:52 +0700

    update update pin

--- server/src/pin/dto/update-pin.dto.ts
+++ server/src/pin/dto/update-pin.dto.ts
@@ -1,13 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Pin } from '@prisma/client'
-import { IsString } from 'class-validator'
+import { IsOptional, IsString } from 'class-validator'
 
 export class UpdatePinDto implements Partial<Pin> {
   @ApiProperty({ required: false })
   @IsString()
+  @IsOptional()
   title?: string
 
   @ApiProperty({ required: false })
   @IsString()
+  @IsOptional()
   description?: string
 }
